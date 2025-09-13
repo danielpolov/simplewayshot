62cb133e98003897eac85d5b433c0e19ea640099
danielpolov <danielpolowow@gmail.com>
2025-09-13 14:19:51 -0500

    Refactoring the code

--- simplewayshot.sh
+++ simplewayshot.sh
@@ -8,8 +8,8 @@ main_opts=(" --extra-button=Screen" " --extra-button=Region")
 main_dir="$(dirname "$(realpath "$0")")"
 
 zenity_opts() {
-	local -n arr=$3
-	zenity --question --text="$1" --title="$2" ${arr[@]} --switch
+	local -n screens=$3
+	zenity --question --text="$1" --title="$2" ${screens[@]} --switch
 }
 
 clean_tmp() {
@@ -20,18 +20,18 @@ clean_tmp() {
 #where they can choose the directory to save the ScreenShot
 save_ss(){
 	where_to_save=$(zenity --file-selection --save --filename=$shotname 2> /dev/null)
-	if [ -w "$(dirname "$where_to_save")" ]; then
-		if [ "$where_to_save" != "" ]; then
-			mv /tmp/$shotname $where_to_save
-			notify-send -i $where_to_save --app-name=$appname "Saving SS as $where_to_save"
-		else
-			clean_tmp
-			notify_user_cancel "You didn't select a Directory. Nothing was saved."
-		fi
-	else
-		zenity --error --title="Fatal Error" --text="You do not have the correct permissions."
-		save_ss
+	if [ -z "$where_to_save" ]; then
+		clean_tmp
+		notify_user_cancel "You didn't select a Directory. Nothing was saved."
+		return
 	fi
+	if [ -w "$(dirname "$where_to_save")" ]; then
+		mv /tmp/$shotname $where_to_save
+		notify-send -i $where_to_save --app-name=$appname "Saving SS as $where_to_save"
+		return
+	fi
+	zenity --error --title="Fatal Error" --text="You do not have the correct permissions."
+	save_ss
 }
 
 notify_user_cancel(){
@@ -41,40 +41,90 @@ notify_user_cancel(){
 #Where the magic happens. using grim to take the Screenshot
 #slurp to get the region of the screen
 #zenity to show the GTK dialog
-option_to_ss=$(zenity_opts "What do you want to ScreenShot" $appname main_opts)
-case $option_to_ss in
-	"Screen")
-		no_screens=$(wlr-randr --json | jq 'map(select((.enabled? // .active? // .connected?) == true)) | length')
-		screen_to_ss=""
-		if [ no_screens -gt 1 ]; then
-			list_of_screens=($(wlr-randr --json | jq -r '.[] | select(.enabled? // .connected? // .active?) | .name'))
-			list_of_options=()
-			for screen in "${list_of_screens[@]}"; do
-				list_of_options+=(' --extra-button='"$screen")
-			done
-			screen_to_ss=$(zenity_opts "Which Screen" $appname list_of_options)
-		else
-			screen_to_ss=$(wlr-randr --json | awk -F'"' '/name/ {print $4}')
-		fi
-		if [ "$screen_to_ss" != "" ]; then
-			grim -o $screen_to_ss $where_to_save$shotname 2> /dev/null
-			save_ss
-		else
-			notify_user_cancel "You didn't select a Screen. Nothing was saved."
-		fi
-		;;
-	"Region")
-		region=$(slurp 2> /dev/null)
-		if [ "$region" != "" ]; then
-			grim -g "$region" $where_to_save$shotname
-			save_ss
-		else
-			notify_user_cancel "You din't select a Region. Nothing was saved."
-		fi
-		;;
-	*)
-		#If the user closes the dialog, this will be executed
-		notify_user_cancel "Exiting SimpleWayshot. No ss was taken."
-		;;
-esac
+take_ss_options(){
+	local option_to_ss=$(zenity_opts "What do you want to ScreenShot" $appname main_opts)
+	case $option_to_ss in
+		"Screen")
+			local no_screens=$(wlr-randr --json | jq 'map(select((.enabled? // .active? // .connected?) == true)) | length')
+			local screen_to_ss=""
+			if [ no_screens -gt 1 ]; then
+				local list_of_screens=($(wlr-randr --json | jq -r '.[] | select(.enabled? // .connected? // .active?) | .name'))
+				local list_of_options=()
+				for screen in "${list_of_screens[@]}"; do
+					list_of_options+=(' --extra-button='"$screen")
+				done
+				screen_to_ss=$(zenity_opts "Which Screen" $appname list_of_options)
+			else
+				screen_to_ss=$(wlr-randr --json | awk -F'"' '/name/ {print $4}')
+			fi
+			if [ ! -z "$screen_to_ss" ]; then
+				grim -o $screen_to_ss $where_to_save$shotname 2> /dev/null
+				save_ss
+			else
+				notify_user_cancel "You didn't select a Screen. Nothing was saved."
+			fi
+			;;
+		"Region")
+			take_ss_region
+			;;
+		*)
+			#If the user closes the dialog, this will be executed
+			notify_user_cancel "Exiting SimpleWayshot. No ss was taken."
+			;;
+	esac
+}
+
+take_ss_screen() {
+	grim $where_to_save$shotname
+	save_ss
+}
+
+take_ss_region() {
+	local region=$(slurp 2> /dev/null)
+	if [ ! -z "$region" ]; then
+		grim -g "$region" $where_to_save$shotname
+		save_ss
+	else
+		notify_user_cancel "You din't select a Region. Nothing was saved."
+	fi
+}
+
+show_help() {
+	cat <<EOF
+	Usage: ${0##*/} [OPTIONS]
+	You can use one of the fllowing options.
+
+	Options:
+	-h,   Show this help message
+	-s,   Uses the main screen to take the screenshot
+	-r,   Select a region to take the ScreenShot
+	-o,   Launches Zenity with two options(Screen or Region). If you have multiple screens, the screen option will prompt you with a list to select the one you want.
+
+	Examples:
+	${0##*/} -s
+EOF
+}
+
+main() {
+	while getopts 'sroh' OPTION; do
+		case "$OPTION" in
+			s)
+				take_ss_screen
+				;;
+			r)
+				take_ss_region
+				;;
+			o)
+				take_ss_options
+				;;
+			h)
+				show_help
+				;;
+			*)
+				zenity --error --text="Invalid Option" --title="SimpleWayshot"
+		esac
+	done
+}
+
+main "$@"
 
